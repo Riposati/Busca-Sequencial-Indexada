fb0881da9a06caa2f37ead8e1960c17a8ceafacd
gurip_000 <gurip_000@192.168.1.105>
2015-08-14 14:58:28 -0300

    teste

---
+++ src/buscasequencialindexada/Dados.java
@@ -0,0 +1,17 @@
+package buscasequencialindexada;
+
+public class Dados{
+    
+    private int chave;
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    
+    
+}
---
+++ src/buscasequencialindexada/Indice.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscasequencialindexada;
+
+/**
+ *
+ * @author gurip_000
+ */
+public class Indice {
+    
+    private int chave;
+    private long entrada;
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    public long getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(long entrada) {
+        this.entrada = entrada;
+    }
+}
